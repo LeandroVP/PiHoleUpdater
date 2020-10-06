c5ef0c0887f929f7f09fefe192b4cb049a9c4f04
LeandroVP <leandrovega@correo.ugr.es>
2020-10-06 21:19:08 +0200

    gitignore updated. Closes #10

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, PiHoleUpdaterApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<PiHoleUpdaterApplication>;
