9de1850118cc9ac67a300d371e76cef0174b0007
LeandroVP <leandrovega@correo.ugr.es>
2020-10-27 19:07:54 +0100

    PiHoleUpdater first class implemented. Closes #19 . Contributes to #6 #7 #8

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
-import { ListItem } from "./models/list.model";
+class PiHoleUpdater {
+  constructor() {
+    this.mainFunction();
+  }
 
-export class main{
+  private path = require("path");
+  private fs = require("fs");
+  private directoryPath = this.path.join(__dirname, "../lists");
+  private fileNames: string[] = [];
+  private filesLines: string[] = [];
 
-    private _list: ListItem[];
-    constructor(){
-        this._list = []
-        
-        console.log('Hello world!');
-    }
+  mainFunction() {
+    //passsing directoryPath and callback function
+    this.fileNames = this.fs.readdirSync(this.directoryPath);
+    
+    this.fileNames.forEach((fileName) => {
+      this.filesLines = this.filesLines.concat([
+        this.fs.readFileSync(this.directoryPath + "/" + fileName, "utf8"),
+      ]);
+    });
+    
+    console.log(this.fileNames);
+    console.log(this.filesLines);
+  }
+
+  storeAddresses() {
+
+    this.fs.writeFile('./build/output.txt', this.filesLines,  function(err:Error) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("File created!");
+    });
+}
+  showAddresses(){
+    console.log(this.fileNames);
+    console.log(this.filesLines);
+  }
 
 }
+
+// Usage
+var obj = new PiHoleUpdater();
+obj.storeAddresses();
+obj.showAddresses();
